f3d84bfadf65ed72d6f61deca7638bd183b1dd3a
jazzocordeiro <cordeirorosadigital@gmail.com>
2024-11-09 07:45:59 -0300

    estudos

--- app.js
+++ app.js
@@ -1,43 +1,91 @@
 import express from 'express';
-import mysql from 'mysql2'
+import mysql from 'mysql2';
 import { engine } from 'express-handlebars';
 import fileupload from 'express-fileupload';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
+// Necessário para funcionar o __dirname com ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middlewares
 app.use(fileupload());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+// Static
 app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
+app.use('/css', express.static('./css'));
+app.use('/imagens', express.static('./imagens')); // para acessar imagens
 
-app.use('/css', express.static('./css'))
 
+
+// View engine
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
+// Banco de dados
 const configDB = mysql.createConnection({
     host: 'localhost',
-    user:'root',
+    user: 'root',
     password: '1234',
     database: 'fullStack'
-})
+});
 
-configDB.connect(function(erro){
-    if(erro) throw erro;
-    console.log('conexão feita com sucesso')
-})
+configDB.connect((erro) => {
+    if (erro) throw erro;
+    console.log('Conexão feita com sucesso');
+});
 
-app.get('/', function(req, res){
-    res.render('formulario')
-})
+// Rota principal
+app.get('/', (req, res) => {
+    const { sucesso, erro } = req.query; // lê os parâmetros da URL
 
-app.post('/cadastrar', function(req, res){
-    console.log(req.body);
-    req.files.imagem.mv(__dirname + 'imagens/' + req.files.imagem.name);
-    res.end();
-})
+    const sql = 'SELECT * FROM produtos';
 
-app.listen(8080)
+    configDB.query(sql, (err, rows) => {
+        if (err) {
+            return res.render('formulario', {
+                erro: 'Erro ao buscar produtos do banco.',
+                produtos: []
+            });
+        }
+
+        res.render('formulario', {
+            sucesso,
+            erro,
+            produtos: rows
+        });
+    });
+});
+
+// Rota de cadastro
+app.post('/cadastrar', (req, res) => {
+    const { nome, valor } = req.body;
+
+    if (!req.files || !req.files.imagem) {
+        return res.redirect('/?erro=Imagem não foi enviada.');
+    }
+
+    const imagem = req.files.imagem;
+    const uploadPath = path.join(__dirname, 'imagens', imagem.name);
+
+    imagem.mv(uploadPath, (err) => {
+        if (err) return res.redirect('/?erro=Erro ao salvar imagem.');
+
+        let sql = 'INSERT INTO produtos (nome, valor, imagem) VALUES (?, ?, ?)';
+        configDB.query(sql, [nome, valor, imagem.name], (err) => {
+            if (err) return res.redirect('/?erro=Erro ao salvar no banco.');
+
+            res.redirect('/?sucesso=Produto cadastrado com sucesso!');
+        });
+    });
+});
+
+app.listen(8080, () => {
+    console.log('Servidor rodando em http://localhost:8080');
+});
